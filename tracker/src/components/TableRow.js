164842389ace91441b802710bd409620d4195cad
Gaurav Agarwal <gaurav.agarwal28.12.96@gmail.com>
2019-10-03 23:19:16 +0530

    Get functionality added for Projects.

---
+++ tracker/src/components/TableRow.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.id}
+          </td>
+          <td>
+            {this.props.obj.name}
+          </td>
+          <td>
+            {this.props.obj.description}
+          </td>
+          <td>
+            {this.props.obj.man_hours}
+          </td>
+          <td>
+            {this.props.obj.image}
+          </td>
+          <td>
+            {this.props.obj.owner}
+          </td>
+          <td>
+            <button className="btn btn-primary">Edit</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
--- tracker/src/components/index.component.js
+++ tracker/src/components/index.component.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { projects: []};
+    }
+
+    componentDidMount() {
+        fetch('https://team7-awaaz.herokuapp.com/project/')
+        .then(response => response.json())
+        .then(projects => this.setState({projects}))
+        .catch(err => console.log(err))
+    }
+
+    tabRow() {
+        return this.state.projects.map(function(object, id) {
+            return <TableRow obj={object} key={id} />;
+        });
+    }
+
     render() {
         return (
-            <div>
-                <p>Welcome to Index Component!!</p>
-            </div>
-        )
+          <div>
+            <h3 align="center">Projects List</h3>
+            <table className="table table-striped" style={{ marginTop: 20 }}>
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Man hours</th>
+                  <th>Image</th>
+                  <th>Owner</th>
+                  <th colSpan="2">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                { this.tabRow() }
+              </tbody>
+            </table>
+          </div>
+        );
     }
 }
